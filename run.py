aa185855d1e260eb2c8bc82376c1f35eb3341984
zhangjc <zhangjc0804@126.com>
2020-10-15 23:19:54 +0800

    路径修改后提交
    Signed-off-by: zhangjc <zhangjc0804@126.com>

--- run.py
+++ run.py
@@ -7,16 +7,16 @@ import pytest
 if __name__ == '__main__':
      # 修改成要执行的测试用例
      test_case1 = './test_case/User/test_sign.py'
-     # test_case2 = './test_case/User/test_login.py'
-     # test_case3 = './test_case/User/test_realname.py'
-     # test_case4 = './test_case/User/test_oldpwd.py'
-     # test_case5 = './test_case/User/test_changepwd.py'
+     test_case2 = './test_case/User/test_login.py'
+     test_case3 = './test_case/User/test_realname.py'
+     test_case4 = './test_case/User/test_oldpwd.py'
+     test_case5 = './test_case/User/test_changepwd.py'
 
      xml_report_path = './report/xml/'
      html_report_path = './report/html/'
 
      pytest.main(['-s', '-q', '--alluredir',
-	      xml_report_path, test_case1])
+	      xml_report_path, test_case1, test_case2, test_case3, test_case4, test_case5])
      cmd1 = 'allure generate %s -o %s --clean' % (xml_report_path, html_report_path)
      cmd2 = 'allure serve %s' % (xml_report_path)
      shell_tool.invoke(cmd1)
